65519b83b5b358459b2d105add6a0877d3d99a12
Raja s <rajaaltus@gmail.com>
2021-02-17 21:12:02 +0700

    mobile view fixed

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,12 @@
   <div>
     <nav class="app-header app-flex">
       <HeaderComponentsLogo />
-      <HeaderComponentsNav />
+      <HeaderComponentsNav class="hidden md:block" />
+      <div class="block md:hidden">
+        <svg class="fill-current w-8 h-8">
+          <path d="M24 6H0V2h24v4zm0 4H0v4h24v-4zm0 8H0v4h24v-4z" />
+        </svg>
+      </div>
     </nav>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bg-gray-800 text-white py-4 pb-4">
-    <section class="mx-auto max-w-7xl">
+    <section class="mx-auto max-w-7xl px-4">
       <h1 class="text-4xl font-semibold mx-auto py-4">Recents</h1>
-      <div class="w-full grid grid-cols-5 gap-4">
+      <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4">
         <MovieCard v-for="movie in movies" :key="movie.id" :movie_id="movie.id" :title="movie.title" :genres="movie.genres" :description="movie.summary" :image="movie.medium_cover_image" :rating="movie.rating" :downloads="movie.torrents" />
       </div>
     </section>
