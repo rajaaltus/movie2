80ce47958ecde5f0b42799d8e1091f27bb5f011b
Raja s <rajaaltus@gmail.com>
2021-02-20 12:02:04 +0700

    mode changed to spa

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,7 @@ export default {
    ** Headers of the page
    */
   target: "static",
+  ssr: false,
   head: {
     title: process.env.npm_package_name || "",
     meta: [{ charset: "utf-8" }, { name: "viewport", content: "width=device-width, initial-scale=1" }, { hid: "description", name: "description", content: process.env.npm_package_description || "" }],
